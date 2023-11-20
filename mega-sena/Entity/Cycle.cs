e2146d7e5e0e397230da3c2a2d841d242ca7f4d1
Alan Jefferson <alan.jefferson@gmail.com>
2023-11-20 19:09:42 -0300

    Add number cycle times

--- mega-sena/Entity/Cycle.cs
+++ mega-sena/Entity/Cycle.cs
@@ -63,6 +63,66 @@
         private bool number58;
         private bool number59;
         private bool number60;
+		private int timesNum01;
+		private int timesNum02;
+		private int timesNum03;
+		private int timesNum04;
+		private int timesNum05;
+		private int timesNum06;
+		private int timesNum07;
+		private int timesNum08;
+		private int timesNum09;
+		private int timesNum10;
+		private int timesNum11;
+		private int timesNum12;
+		private int timesNum13;
+		private int timesNum14;
+		private int timesNum15;
+		private int timesNum16;
+		private int timesNum17;
+		private int timesNum18;
+		private int timesNum19;
+		private int timesNum20;
+		private int timesNum21;
+		private int timesNum22;
+		private int timesNum23;
+		private int timesNum24;
+		private int timesNum25;
+		private int timesNum26;
+		private int timesNum27;
+		private int timesNum28;
+		private int timesNum29;
+		private int timesNum30;
+		private int timesNum31;
+		private int timesNum32;
+		private int timesNum33;
+		private int timesNum34;
+		private int timesNum35;
+		private int timesNum36;
+		private int timesNum37;
+		private int timesNum38;
+		private int timesNum39;
+		private int timesNum40;
+		private int timesNum41;
+		private int timesNum42;
+		private int timesNum43;
+		private int timesNum44;
+		private int timesNum45;
+		private int timesNum46;
+		private int timesNum47;
+		private int timesNum48;
+		private int timesNum49;
+		private int timesNum50;
+		private int timesNum51;
+		private int timesNum52;
+		private int timesNum53;
+		private int timesNum54;
+		private int timesNum55;
+		private int timesNum56;
+		private int timesNum57;
+		private int timesNum58;
+		private int timesNum59;
+		private int timesNum60;
 
 		public DateTime? EndCycle { get => endCycle; set => endCycle = value; }
 		public bool Number01 { get => number01; set => number01 = value; }
@@ -125,5 +185,65 @@
 		public bool Number58 { get => number58; set => number58 = value; }
 		public bool Number59 { get => number59; set => number59 = value; }
 		public bool Number60 { get => number60; set => number60 = value; }
+		public int TimesNum01 { get => timesNum01; set => timesNum01 = value; }
+		public int TimesNum02 { get => timesNum02; set => timesNum02 = value; }
+		public int TimesNum03 { get => timesNum03; set => timesNum03 = value; }
+		public int TimesNum04 { get => timesNum04; set => timesNum04 = value; }
+		public int TimesNum05 { get => timesNum05; set => timesNum05 = value; }
+		public int TimesNum06 { get => timesNum06; set => timesNum06 = value; }
+		public int TimesNum07 { get => timesNum07; set => timesNum07 = value; }
+		public int TimesNum08 { get => timesNum08; set => timesNum08 = value; }
+		public int TimesNum09 { get => timesNum09; set => timesNum09 = value; }
+		public int TimesNum10 { get => timesNum10; set => timesNum10 = value; }
+		public int TimesNum11 { get => timesNum11; set => timesNum11 = value; }
+		public int TimesNum12 { get => timesNum12; set => timesNum12 = value; }
+		public int TimesNum13 { get => timesNum13; set => timesNum13 = value; }
+		public int TimesNum14 { get => timesNum14; set => timesNum14 = value; }
+		public int TimesNum15 { get => timesNum15; set => timesNum15 = value; }
+		public int TimesNum16 { get => timesNum16; set => timesNum16 = value; }
+		public int TimesNum17 { get => timesNum17; set => timesNum17 = value; }
+		public int TimesNum18 { get => timesNum18; set => timesNum18 = value; }
+		public int TimesNum19 { get => timesNum19; set => timesNum19 = value; }
+		public int TimesNum20 { get => timesNum20; set => timesNum20 = value; }
+		public int TimesNum21 { get => timesNum21; set => timesNum21 = value; }
+		public int TimesNum22 { get => timesNum22; set => timesNum22 = value; }
+		public int TimesNum23 { get => timesNum23; set => timesNum23 = value; }
+		public int TimesNum24 { get => timesNum24; set => timesNum24 = value; }
+		public int TimesNum25 { get => timesNum25; set => timesNum25 = value; }
+		public int TimesNum26 { get => timesNum26; set => timesNum26 = value; }
+		public int TimesNum27 { get => timesNum27; set => timesNum27 = value; }
+		public int TimesNum28 { get => timesNum28; set => timesNum28 = value; }
+		public int TimesNum29 { get => timesNum29; set => timesNum29 = value; }
+		public int TimesNum30 { get => timesNum30; set => timesNum30 = value; }
+		public int TimesNum31 { get => timesNum31; set => timesNum31 = value; }
+		public int TimesNum32 { get => timesNum32; set => timesNum32 = value; }
+		public int TimesNum33 { get => timesNum33; set => timesNum33 = value; }
+		public int TimesNum34 { get => timesNum34; set => timesNum34 = value; }
+		public int TimesNum35 { get => timesNum35; set => timesNum35 = value; }
+		public int TimesNum36 { get => timesNum36; set => timesNum36 = value; }
+		public int TimesNum37 { get => timesNum37; set => timesNum37 = value; }
+		public int TimesNum38 { get => timesNum38; set => timesNum38 = value; }
+		public int TimesNum39 { get => timesNum39; set => timesNum39 = value; }
+		public int TimesNum40 { get => timesNum40; set => timesNum40 = value; }
+		public int TimesNum41 { get => timesNum41; set => timesNum41 = value; }
+		public int TimesNum42 { get => timesNum42; set => timesNum42 = value; }
+		public int TimesNum43 { get => timesNum43; set => timesNum43 = value; }
+		public int TimesNum44 { get => timesNum44; set => timesNum44 = value; }
+		public int TimesNum45 { get => timesNum45; set => timesNum45 = value; }
+		public int TimesNum46 { get => timesNum46; set => timesNum46 = value; }
+		public int TimesNum47 { get => timesNum47; set => timesNum47 = value; }
+		public int TimesNum48 { get => timesNum48; set => timesNum48 = value; }
+		public int TimesNum49 { get => timesNum49; set => timesNum49 = value; }
+		public int TimesNum50 { get => timesNum50; set => timesNum50 = value; }
+		public int TimesNum51 { get => timesNum51; set => timesNum51 = value; }
+		public int TimesNum52 { get => timesNum52; set => timesNum52 = value; }
+		public int TimesNum53 { get => timesNum53; set => timesNum53 = value; }
+		public int TimesNum54 { get => timesNum54; set => timesNum54 = value; }
+		public int TimesNum55 { get => timesNum55; set => timesNum55 = value; }
+		public int TimesNum56 { get => timesNum56; set => timesNum56 = value; }
+		public int TimesNum57 { get => timesNum57; set => timesNum57 = value; }
+		public int TimesNum58 { get => timesNum58; set => timesNum58 = value; }
+		public int TimesNum59 { get => timesNum59; set => timesNum59 = value; }
+		public int TimesNum60 { get => timesNum60; set => timesNum60 = value; }
 	}
 }
